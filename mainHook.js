59eb2f4e259e896ee26b387ff84f2332d442e276
Nick Stracke <nick.stracke@web.de>
2022-04-23 22:01:05 +0200

    add cors

--- mainHook.js
+++ mainHook.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const router = require('./router');
+const cors = require('cors');
+
 
 const app = express();
 
 
 app.use(bodyParser.json());
+app.use(cors())
 router(app);
 
 const PORT = process.env.PORT || 12345;
