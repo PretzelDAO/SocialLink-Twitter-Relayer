d891badc1b8cf4ccbfaa3ed5197e36344d0aefac
Nick Stracke <nick.stracke@web.de>
2022-04-23 11:31:41 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const Twit = require('twit')
+
+const T = new Twit({
+    consumer_key: 'WC1YLXJuQXJybHFNMGQxRmYzNTA6MTpjaQ',
+    consumer_secret: 'hEc5vQZUTVYcF82ed0N6qW8HhlPJEBn0zoLowbIjDQYzuBS-qs',
+    access_token: '...',
+    access_token_secret: '...',
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL: true,     // optional - requires SSL certificates to be valid.
+})
+
+
+// API Key
+// FlbiAYxks0S30qDeq3qUo8bQf
+
+// API Key Secret
+// nbczUsLrEfjpQCHvxrsjYsOjJDEgSPtpOWSqXlmbBKrqgLVAN8
+
+// Brearer Token
+//AAAAAAAAAAAAAAAAAAAAAPwlbwEAAAAAoJrs1%2BFweMU6zpXoD6alAO%2F%2F9NQ%3Ds60OpMFXc6NQaTpfo1YWe6WL2tqoZPPkyXQfbrOaDWNkbLDOXr
